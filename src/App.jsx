89dad81ee167d1bcafad3bb28f8460b4d169957b
Ben Brownsell <ben.brownsell@hotmail.co.uk>
2023-06-04 23:37:23 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+import BtnContainer from "./BtnContainer";
 import JobInfo from "./JobInfo";
 const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0);
 
   const fetchJobs = async () => {
     const response = await fetch(url);
@@ -27,7 +29,12 @@ const App = () => {
 
   return (
     <section className="jobs-center">
-      <JobInfo jobs={jobs} />
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      <JobInfo jobs={jobs} currentItem={currentItem} />
     </section>
   );
 };
